d404d8a12953516e4d4b9073fa140bec96240c85
PATIL-ABHIJEET <abhijeetpatil24922@gmail.com>
2026-08-07 16:14:59 +0530

    Day25

---
+++ HQ9Plus.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HQ9Plus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String program = sc.next();
+
+        if (program.contains("H") || program.contains("Q") || program.contains("9")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
