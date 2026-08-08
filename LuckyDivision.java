ed0e2898d989057105e69e30c1fb84b20ba46529
PATIL-ABHIJEET <abhijeetpatil24922@gmail.com>
2026-08-08 19:51:17 +0530

    Day26

---
+++ LuckyDivision.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LuckyDivision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] lucky = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477,
+                       744, 747, 774, 777};
+
+        for (int number : lucky) {
+            if (n % number == 0) {
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
