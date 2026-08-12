f98990d1bc02a2ea58a2c2f3c60537b47556529e
PATIL-ABHIJEET <abhijeetpatil24922@gmail.com>
2026-08-12 16:27:34 +0530

    Day30

---
+++ Dubstep.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Dubstep {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        s = s.replace("WUB", " ");
+
+        System.out.println(s.trim().replaceAll(" +", " "));
+    }
+}
