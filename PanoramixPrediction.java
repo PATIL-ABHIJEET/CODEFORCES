dc73007ab41b26e46aa918dcbc458f8fbec7579a
PATIL-ABHIJEET <abhijeetpatil24922@gmail.com>
2026-09-02 17:30:46 +0530

    Day 51

---
+++ PanoramixPrediction.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class PanoramixPrediction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        for(int i = n+1; i <= m; i++) {
+            boolean isPrime = true;
+            for(int j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    isPrime = false;
+                    break;
+                    
+                }
+            }
+
+        if(isPrime){
+            if(i == m) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+                break;
+            }
+        }
+        }
+    }
+}
